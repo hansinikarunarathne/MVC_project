48200bdc558d4afb7b0106a63bcc9ea2648be887
hansinikarunarathne <107214435+hansinikarunarathne@users.noreply.github.com>
2022-12-26 11:15:49 +0530

    enter

--- index.php
+++ index.php
@@ -4,4 +4,6 @@
     define('ROOT', dirname(__FILE__));
     $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) :[];
     require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
+
+    
     
